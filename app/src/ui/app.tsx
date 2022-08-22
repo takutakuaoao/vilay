f03be3fedc36e4eda50c9d8ff02e4fc427e55cb9
takuya.aotsuka <takuya.aotsuka@micro-wave.net>
2022-08-22 19:10:00 +0900

    install lexical

--- app/src/ui/app.tsx
+++ app/src/ui/app.tsx
@@ -1,10 +1,41 @@
 import * as React from 'react'
+import {$getRoot, $getSelection, EditorState} from 'lexical';
+import {LexicalComposer} from '@lexical/react/LexicalComposer'
+import {PlainTextPlugin} from '@lexical/react/LexicalPlainTextPlugin';
+import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import {OnChangePlugin} from '@lexical/react/LexicalOnChangePlugin';
+import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
+import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 
 export const App = () => {
+  const onChange = (editorState: EditorState) => {
+    editorState.read(() => {
+      const root = $getRoot();
+      const selection = $getSelection();
+
+      console.log(root, selection);
+    });
+  }
+
+  const MyCustomAutoFocusPlugin = () => {
+    const [editor] = useLexicalComposerContext();
+    React.useEffect(() => {
+      editor.focus();
+    }, [editor]);
+
+    return null;
+  }
+
+  const initialConfig = {
+    namespace: 'MyEditor',
+    onError: (error: Error) => console.error(error),
+  }
   return (
-    <>
-      <div>test</div>
-      <div>testlllllkklll</div>
-    </>
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin contentEditable={<ContentEditable />} placeholder={<div>テキストを入力</div>} />
+      <OnChangePlugin onChange={onChange} />
+      <HistoryPlugin />
+      <MyCustomAutoFocusPlugin />
+    </LexicalComposer>
   )
 }
