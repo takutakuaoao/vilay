5c533e8ac0de7a7832ca5ef1255e0fda75e49323
takuya.aotsuka <takuya.aotsuka@micro-wave.net>
2022-09-02 15:37:07 +0900

    🎨[Clean] Change editor component.
    From lexical component to codemirror.

--- app/src/editor/ui/editor.tsx
+++ app/src/editor/ui/editor.tsx
@@ -1,49 +1,28 @@
 import * as React from 'react'
-import { ContentEditable } from '@lexical/react/LexicalContentEditable'
-import { PlainTextPlugin } from '@lexical/react/LexicalPlainTextPlugin'
-import { AutoFocus } from '../features/auto-focus/auto-focus'
-
-import { RowNumber } from '../features/row-number/row-number'
-import { useCursorRowHighlight } from '../features/cursor-row-highlight/cursor-row-highlight'
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
+import { EditorState } from '@codemirror/state'
+import { EditorView, keymap } from '@codemirror/view'
+import { defaultKeymap } from '@codemirror/commands'
 
 type Props = {
   width: number
 }
 
 export const Editor = ({ width }: Props) => {
-  const paddingTop = 'pt-4'
-  const { cursorHighlight, currentRowNumber } = useCursorRowHighlight()
-  const editor = useLexicalComposerContext()
-
-  React.useEffect(cursorHighlight, [editor])
+  const parent = React.useRef<HTMLDivElement>(null)
+  React.useEffect(() => {
+    const startState = EditorState.create({
+      doc: 'Hello World',
+      extensions: [keymap.of(defaultKeymap)],
+    })
+    new EditorView({
+      state: startState,
+      parent: parent.current!,
+    })
+  }, [])
 
   return (
     <>
-      <div id="default-vilay-typography" className="typography flex-grow">
-        <div id="default-vilay-editor-theme" className="theme">
-          <div className="flex">
-            <RowNumber
-              className={`flex flex-col ${paddingTop} px-4 text-right theme-line-number`}
-              option={{
-                cursorRowNumber: currentRowNumber,
-                cursorHighLightWidth: width,
-              }}
-            />
-            <PlainTextPlugin
-              contentEditable={
-                <ContentEditable
-                  testid="editor"
-                  className={`min-h-screen ${paddingTop} flex-grow px-4 pb-4 overflow-x-auto focus-visible:outline-none w-56`}
-                  id="editor"
-                />
-              }
-              placeholder=""
-            />
-          </div>
-        </div>
-      </div>
-      <AutoFocus />
+      <div id="code-mirror" ref={parent}></div>
     </>
   )
 }
