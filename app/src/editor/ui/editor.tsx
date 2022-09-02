3f2f0002cf4ef16c96ce5517b9089458b5785515
takuya.aotsuka <takuya.aotsuka@micro-wave.net>
2022-09-02 19:09:09 +0900

    👍[Update] Add default theme.
    Add one dark theme as default for codemirror.

--- app/src/editor/ui/editor.tsx
+++ app/src/editor/ui/editor.tsx
@@ -2,23 +2,25 @@ import * as React from 'react'
 import { EditorState } from '@codemirror/state'
 import { EditorView, keymap } from '@codemirror/view'
 import { defaultKeymap } from '@codemirror/commands'
+import { oneDark as defaultTheme } from '@codemirror/theme-one-dark'
 
-type Props = {
-  width: number
-}
-
-export const Editor = ({ width }: Props) => {
+export const Editor = () => {
   const parent = React.useRef<HTMLDivElement>(null)
+
   React.useEffect(() => {
     const startState = EditorState.create({
-      doc: '',
-      extensions: [keymap.of(defaultKeymap)],
+      doc: undefined,
+      extensions: [keymap.of(defaultKeymap), defaultTheme],
     })
-    new EditorView({
+    const editorView = new EditorView({
       state: startState,
       parent: parent.current!,
     })
-  }, [])
+
+    return () => {
+      editorView.destroy()
+    }
+  }, [parent])
 
   return (
     <>
--- app/src/workspace/app.tsx
+++ app/src/workspace/app.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { LexicalComposer } from '@lexical/react/LexicalComposer'
+// import { LexicalComposer } from '@lexical/react/LexicalComposer'
 import { Editor } from '../editor/ui/editor'
 import { FileTree } from '../features/file-tree/ui/file-tree'
 
@@ -7,30 +7,17 @@ import '../../styles/theme/workspace/default-vilay/default-vilay-workspace-theme
 import '../../styles/theme/editor/default-vilay/default-vilay-editor-theme.scss'
 import '../../styles/typography/editor/default-vilay/default-vilay-editor-typography.scss'
 import '../../styles/app.scss'
-// import { Layout } from './features/layout/layout'
 
 const { versions } = window
 
 export const App = () => {
-  const initialConfig = {
-    namespace: 'Editor',
-    onError: (error: Error) => {
-      console.error('cause error')
-      console.error(error)
-    },
-  }
-
   versions.node().then(value => console.log(value))
-
-  // console.log(Layout.factory())
 
   return (
     <div id="default-vilay-workspace-theme">
       <div className="flex">
         <FileTree addingClassName="w-[300px]" />
-        <LexicalComposer initialConfig={initialConfig}>
-          <Editor width={200} />
-        </LexicalComposer>
+        <Editor />
       </div>
     </div>
   )
