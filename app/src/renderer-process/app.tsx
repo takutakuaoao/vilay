78b824424733fcff517eca576ddfdaf4d255459d
aochan <nakinagaraodoru1013@gmail.com>
2022-10-05 19:28:26 +0900

    🔥 サイドバーを削除した

--- app/src/renderer-process/app.tsx
+++ app/src/renderer-process/app.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Editor } from './features/editor/ui/editor'
-import { FileTree } from './features/file-tree/ui/file-tree'
 
 import '../../styles/theme/workspace/default-vilay/default-vilay-workspace-theme.scss'
 import '../../styles/typography/editor/default-vilay/default-vilay-editor-typography.scss'
@@ -14,14 +13,12 @@ export const App = () => {
   electron.node().then(value => console.log(value))
   // const content = useSelector(state => state.note.content)
 
-  const sideMinWidthClass = `min-w-[300px]`
   const editorWidthClass = `w-[calc(100%-300px)]`
 
   return (
     <div id="default-vilay-workspace-theme">
       <div id="default-vilay-typography">
         <div className="flex w-screen">
-          <FileTree addingClassName={sideMinWidthClass} />
           <Editor addClass={editorWidthClass} />
           <Preview />
         </div>
